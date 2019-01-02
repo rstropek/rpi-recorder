571ab8ef291a281f6a62cd938be10cb1dfe04461
Rainer <rainer@software-architects.at>
2019-01-02 16:54:04 +0000

    Add share for recording

--- utils/start-samba.sh
+++ utils/start-samba.sh
@@ -2,6 +2,8 @@
 docker run -d -p 445:445 \
   --restart always \
   -v ~/rpi-recorder:/share/rpi-recorder \
+  -v ~/recordings:/share/recordings \
   --name samba trnape/rpi-samba \
   -u "dev:dev" \
-  -s "rpi-recorder:/share/rpi-recorder:rw:dev"
+  -s "rpi-recorder:/share/rpi-recorder:rw:dev" \
+  -s "recordings:/share/recordings:rw:dev" 
